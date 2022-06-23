9fe33c324e1e6971784527de5c2a6f6e7759ac17
Giles Dring <giles@dringtech.com>
2022-06-23 14:17:26 +0100

    Move gtfs filter to module

---
+++ otp4gb/gtfs_filter.py
@@ -0,0 +1,50 @@
+import logging
+import operator
+import os
+import shutil
+import subprocess
+
+from otp4gb.config import BIN_DIR, MAX_HEAP
+
+logger = logging.getLogger(__name__)
+
+
+def filter_gtfs_files(input_dir, output_dir, date=None, extents=None):
+    location = ":".join([str(x) for x in operator.itemgetter(
+        'min_lat', 'min_lon', 'max_lat', 'max_lon')(extents)])
+    logger.debug(location)
+
+    timetable_files = [os.path.join(input_dir, f)
+                       for f in os.listdir(input_dir)]
+
+    for timetable_file in timetable_files:
+        gtfs_filter(timetable_file,
+                    output_dir=output_dir,
+                    location_filter=location,
+                    date_filter=date
+                    )
+
+
+def gtfs_filter(timetable_file, output_dir, location_filter, date_filter):
+    logger.debug(timetable_file)
+    temp_folder = 'zip_tmp'
+    jar_file = os.path.join(BIN_DIR, 'gtfs-filter-0.1.jar')
+
+    name_base = os.path.splitext(os.path.basename(timetable_file))[0]
+    logger.info('Processing GTFS file %s', name_base)
+    output_file = os.path.join(output_dir, name_base + '_filtered')
+
+    command = [
+        'java',
+        '-Xmx{}'.format(MAX_HEAP),
+        '-jar', jar_file,
+        timetable_file,
+        '-d', date_filter,
+        '-l', location_filter,
+        '-o', temp_folder
+    ]
+
+    logger.debug(command)
+    subprocess.run(' '.join(command), shell=True)
+    shutil.make_archive(output_file, 'zip', temp_folder)
+    shutil.rmtree(temp_folder)
--- prepare.py
+++ prepare.py
@@ -1,13 +1,12 @@
 import getopt
 import logging
-import operator
 import os
 import shutil
-import subprocess
 import sys
+from otp4gb.gtfs_filter import filter_gtfs_files
 
 from otp4gb.osmconvert import osm_convert
-from otp4gb.config import BIN_DIR, CONF_DIR, MAX_HEAP
+from otp4gb.config import CONF_DIR
 from otp4gb.otp import prepare_graph
 from otp4gb.bounds import bounds
 
@@ -52,11 +51,13 @@ def main():
 
         # We need to crop the osm.pbf file and all of the GTFS public transport files for GB
         # And then put them all in one folder, which we then use to run an open trip planner instance.
+        # see https://github.com/odileeds/ATOCCIF2GTFS for timetable files
+        # These need to reside in base_folder/input/gtfs
+        filter_gtfs_files(os.path.join(input_dir, 'gtfs'),
+                          output_dir=filtered_graph_folder,
+                          date=date_filter_string,
+                          extents=extents)
 
-        filter_inputs(input_dir,
-                      output_dir=filtered_graph_folder,
-                      date=date_filter_string,
-                      extents=extents)
         # Crop the osm.pbf map of GB to the bounding box
         # If you are not using Windows a version of osmconvert on your platform may be available via https://wiki.openstreetmap.org/wiki/Osmconvert
         osm_convert(os.path.join(input_dir, 'great-britain-latest.osm.pbf'),
@@ -74,51 +75,6 @@ def main():
             'A graph.obj file already exists and will be used. To rebuild the transport graph delete the graph.obj file.')
     else:
         prepare_graph(filtered_graph_folder)
-
-
-def filter_inputs(input_dir, output_dir, date=None, extents=None):
-    location = ":".join([str(x) for x in operator.itemgetter(
-        'min_lat', 'min_lon', 'max_lat', 'max_lon')(extents)])
-    logging.debug(location)
-
-    # see https://github.com/odileeds/ATOCCIF2GTFS for timetable files
-    # These need to reside in base_folder/input
-
-    gtfs_input_dir = os.path.join(input_dir, 'gtfs')
-    timetable_files = [os.path.join(gtfs_input_dir, f)
-                       for f in os.listdir(gtfs_input_dir)]
-    logging.debug(timetable_files)
-
-    for timetable_file in timetable_files:
-        gtfs_filter(os.path.join(input_dir, timetable_file),
-                    output_dir=output_dir,
-                    location_filter=location,
-                    date_filter=date
-                    )
-
-
-def gtfs_filter(timetable_file, output_dir, location_filter, date_filter):
-    temp_folder = 'zip_tmp'
-    jar_file = os.path.join(BIN_DIR, 'gtfs-filter-0.1.jar')
-
-    name_base = os.path.splitext(os.path.basename(timetable_file))[0]
-    logging.debug(name_base)
-    output_file = os.path.join(output_dir, name_base + '_filtered')
-
-    command = [
-        'java',
-        '-Xmx{}'.format(MAX_HEAP),
-        '-jar', jar_file,
-        timetable_file,
-        '-d', date_filter,
-        '-l', location_filter,
-        '-o', temp_folder
-    ]
-
-    logging.debug(command)
-    subprocess.run(' '.join(command), shell=True)
-    shutil.make_archive(output_file, 'zip', temp_folder)
-    shutil.rmtree(temp_folder)
 
 
 if __name__ == '__main__':
