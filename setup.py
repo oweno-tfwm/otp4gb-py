5fac838fcbf9cc1558aa432bca97457362b5ab93
Giles Dring <giles@dringtech.com>
2022-06-29 17:17:06 +0100

    Fix issue with setup

--- setup.py
+++ setup.py
@@ -7,11 +7,12 @@ from subprocess import run
 MAVEN_PATH=os.path.join('bin', 'apache-maven-3.8.6', 'bin', 'mvn')
 
 def setup():
-    urlretrieve('http://m.m.i24.cc/osmconvert64.exe', 'bin/osmconvert64.exe')
+    print('Download OSM convert with large file support from https://wiki.openstreetmap.org/wiki/Osmconvert#Binaries')
+    urlretrieve('https://yadi.sk/d/Vnwc4kut3LCBFm', 'bin/osmconvert64.exe')
     urlretrieve('https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.zip', 'bin/maven.zip')
     with zipfile.ZipFile('bin/maven.zip', 'r') as zip_ref:
       zip_ref.extractall('bin')
-    run('{mvn} dependency:copy-dependencies copy-rename:rename'.format(mvn=MAVEN_PATH), shell=True)
+    run('{mvn} dependency:copy-dependencies copy-rename:rename'.format(mvn=MAVEN_PATH), shell=True, check=True)
 
 if __name__ == '__main__':
     setup()
