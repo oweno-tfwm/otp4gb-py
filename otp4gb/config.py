1a7f04ef72c6b4692b9e1197dab952cc9065b444
Giles Dring <giles@dringtech.com>
2022-06-29 23:30:16 +0100

    Increase size of server

--- otp4gb/config.py
+++ otp4gb/config.py
@@ -11,7 +11,7 @@ LOG_DIR = os.path.join(ROOT_DIR, 'logs')
 
 # if you're running on a virtual machine (no virtual memory/page disk) this must not exceed the total amount of RAM.
 PREPARE_MAX_HEAP = os.environ.get('PREPARE_MAX_HEAP', '20G')
-SERVER_MAX_HEAP = os.environ.get('SERVER_MAX_HEAP', '2G')
+SERVER_MAX_HEAP = os.environ.get('SERVER_MAX_HEAP', '20G')
 
 
 def load_config(dir):
